89b163f8df759e2a7b237cbd5c6879b895ca9689
Keks <keks@htmlacademy.ru>
2023-03-21 12:12:40 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/script.js
+++ 4/js/script.js
@@ -10435,7 +10435,7 @@ const ScreenNumber = {
 class FullPageScroll {
   constructor() {
     this.THROTTLE_TIMEOUT = 1000;
-    this.CURTAIN_TIMEOUT = 300;
+    this.CURTAIN_TIMEOUT = 500;
     this.scrollFlag = true;
     this.timeout = null;
 
